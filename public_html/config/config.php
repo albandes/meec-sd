f31cf9d6bfa023ec44266ec645fb65ef0f510a86
Rogério Albandes <rogerio.albandes@gmail.com>
2025-04-21 18:10:15 -0300

    Move to config.php

---
+++ public_html/config/config.php
@@ -0,0 +1,6 @@
+<?php
+header('Content-Type: application/javascript');
+
+// Aqui você pode aplicar controle de acesso (IP, sessão, etc.)
+
+echo "const MQTT_BROKER_URL = \"ws://54.235.114.156:9001\";";
